b2a87465d1ddfe60bfb513fac28493bd77e526bc
Gary Tietjen <gary@garyt.me>
2023-04-21 14:15:59 -0400

    update portfolio to hide stack items

--- src/components/Templates/PortfolioTemplate.tsx
+++ src/components/Templates/PortfolioTemplate.tsx
@@ -86,6 +86,7 @@ export default function PortfolioTemplate({
                   stack_item: '',
                 },
               ],
+              hidden: false,
             },
           ],
         },
